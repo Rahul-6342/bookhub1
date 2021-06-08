98ed25107881b8f0b342f8f0b2d067c706c68581
Rahul A <rahula6342@gmail.com>
2021-06-08 21:33:35 +0530

    commit

--- app/src/main/java/fragment/DashboardFragment.kt
+++ app/src/main/java/fragment/DashboardFragment.kt
@@ -1,5 +1,7 @@
 package fragment
 
+import adapter.DashboardRecyclerAdapter
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -12,12 +14,16 @@ import com.rahul.bookhub.R
 class DashboardFragment : Fragment() {
     lateinit var recyclerDashboard:RecyclerView
     lateinit var layoutManager:RecyclerView.LayoutManager
+    lateinit var RecyclerAdapter:DashboardRecyclerAdapter
+    val bookList= arrayListOf("jithu","Rahul","praveen","anees","anandhu","greg","basil")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_dashboard, container, false)
+        RecyclerAdapter= DashboardRecyclerAdapter(activity as Context,bookList)
+        val view=inflater.inflate(R.layout.fragment_dashboard, container, false)
         recyclerDashboard=view.findViewById(R.id.recyclerDashboard)
         layoutManager=LinearLayoutManager(activity)
+        return view
     }
 }
